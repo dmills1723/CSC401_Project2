24d545576e73fa64989d5487355f0d19a4cc7f83
Anuraag Agarwal <anuraagagarwal@Anuraags-MacBook-Pro.local>
2019-03-22 16:37:49 -0400

    updated p2mpServer.py

--- p2mpServer.py
+++ p2mpServer.py
@@ -1,7 +1,7 @@
 import sys
 import socket
-import random
-from bitstring import BitArray, BitStream
+import numpy as np
+from bitstring import BitArray
 
 
 def getArgs(args):
@@ -19,13 +19,13 @@ def getArgs(args):
 
 def getIPAddress():
     # Creates socket to Google's nameserver.
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.connect(("8.8.8.8", 80))
+    sockIP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sockIP.connect(("8.8.8.8", 80))
 
     # Gets this computer's IP address from the socket connection.
-    ip_addr = sock.getsockname()[0]
+    ip_addr = sockIP.getsockname()[0]
 
-    sock.close()
+    sockIP.close()
     return ip_addr
 
 
@@ -53,50 +53,64 @@ seqnum = 0
 checksum = 0
 headercheck = 0
 ACK = None
-lastACK = None
+prevACK = None
+lastPacket = False
 
-while True:
-    data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
-    print("Message:", data)
+try:
+    while True:
+        data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+        print("Message:", data)
 
-    # generate random number for packet loss
-    rand = random.uniform(0, 1)
+        # generate random number for packet loss
+        rand = np.random.uniform(0, 1)
+        print(rand)
 
-    # continue if > p, otherwise drop packet
-    if rand > p:
+        # continue if > p, otherwise drop packet
+        if rand > P_LOSS:
 
-        # stop-and-wait processing rules - get data
-        bits = BitArray(data)
+            # stop-and-wait processing rules - get data
+            bits = BitArray(data)
 
-        # compute checksum using utility function
-        headercheck = 0
+            # compute checksum using utility function
+            headercheck = 0
 
-        # check in-sequence
-        seqnum = bits[:32]
-        if seqnum == (prevseqnum + 1):
-            inseq = True
-        else:
-            inseq = False
+            # check in-sequence
+            seqnum = bits[:32]
+            if seqnum == (prevseqnum + 1):
+                inseq = True
+            else:
+                inseq = False
 
-        prevseqnum = seqnum
+            prevseqnum = seqnum
 
-        # check checksum value
-        checksum = bits[32:48]
-        if checksum == headercheck:
-            check = True
-        else:
-            check = False
+            # check checksum value
+            checksum = bits[32:48]
+            if checksum == headercheck:
+                check = True
+            else:
+                check = False
 
-        # if checksum correct and in-sequence, send ACK segment for packet to client (UDP)
-        if inseq and check:
-            # send ACK for packet
-            # sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-            # then, write received data to file
-            file = open(file, 'wb')
-            databits = bits[64:]
-            BitArray(databits).tofile(file)
-            file.close()
+            # SUCCESS: if checksum correct and in-sequence, send ACK segment for packet to client (UDP)
+            if inseq and check:
+                # send ACK for packet
+                sock.sendto(str.encode('hi'), addr)
+                prevACK = ACK
+                # then, write received data to file
+                file = open(FILE_NAME, 'wb')
+                databits = bits[64:]
+                BitArray(databits).tofile(file)
+                file.close()
 
-        # if checksum correct and out-of-sequence, send ACK for last received in-sequence packet to client (UDP)
-        if not inseq and check:
-            # sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+            # if checksum correct and out-of-sequence, send ACK for last received in-sequence packet to client (UDP)
+            if not inseq and check:
+                sock.sendto(str.encode('hi'), addr)
+
+        if lastPacket:
+            break
+
+    print("Successfully exited program\n")
+    sys.exit(0)
+
+except KeyboardInterrupt:
+    print("\nSuccessfully exited program\n")
+    sys.exit(0)
