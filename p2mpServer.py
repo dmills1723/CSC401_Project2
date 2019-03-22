8d98f13529badb90474731c78e90439ca940dd29
Anuraag Agarwal <anuraagagarwal@Anuraags-MacBook-Pro.local>
2019-03-22 16:06:24 -0400

    updated

--- p2mpServer.py
+++ p2mpServer.py
@@ -1,5 +1,7 @@
 import sys
 import socket
+import random
+from bitstring import BitArray, BitStream
 
 
 def getArgs(args):
@@ -31,9 +33,9 @@ def getIPAddress():
 success, port, file, p = getArgs(sys.argv)
 
 if success:
-    SERVER_PORT = port
+    SERVER_PORT = int(port)
     FILE_NAME = file
-    P_LOSS = p
+    P_LOSS = float(p)
 else:
     sys.exit(1)
 
@@ -42,17 +44,59 @@ SERVER_IP = getIPAddress()
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((SERVER_IP, SERVER_PORT))
 
+print("socket success\n")
+
+inseq = False
+check = False
+prevseqnum = -1
+seqnum = 0
+checksum = 0
+headercheck = 0
+ACK = None
+lastACK = None
+
 while True:
     data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
     print("Message:", data)
 
-    # compute checksum
+    # generate random number for packet loss
+    rand = random.uniform(0, 1)
 
-    # check if in-sequence
+    # continue if > p, otherwise drop packet
+    if rand > p:
 
-    # if checksum incorrect, do nothing
+        # stop-and-wait processing rules - get data
+        bits = BitArray(data)
 
-    # if checksum correct and in-sequence, send ACK segment for packet to client (UDP)
-    # then, write received data to file
+        # compute checksum using utility function
+        headercheck = 0
 
-    # if checksum correct and out-of-sequence, send ACK for last received in-sequence packet to client (UDP)
+        # check in-sequence
+        seqnum = bits[:32]
+        if seqnum == (prevseqnum + 1):
+            inseq = True
+        else:
+            inseq = False
+
+        prevseqnum = seqnum
+
+        # check checksum value
+        checksum = bits[32:48]
+        if checksum == headercheck:
+            check = True
+        else:
+            check = False
+
+        # if checksum correct and in-sequence, send ACK segment for packet to client (UDP)
+        if inseq and check:
+            # send ACK for packet
+            # sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+            # then, write received data to file
+            file = open(file, 'wb')
+            databits = bits[64:]
+            BitArray(databits).tofile(file)
+            file.close()
+
+        # if checksum correct and out-of-sequence, send ACK for last received in-sequence packet to client (UDP)
+        if not inseq and check:
+            # sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
