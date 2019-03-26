c4b29baaa8abee23f268e06bd2bc1c79b62e2a5c
demills <demills@ncsu.edu>
2019-03-26 18:10:39 -0400

    Refactored utils.py for efficiency.

--- utils.py
+++ utils.py
@@ -1,84 +1,75 @@
-import bitstring
-import sys
-import time
-from bitstring import BitArray
-
 # The constant "1010101010101010" used as a header indicating the packet is an ACK.
-ACK_PACKET_HEADER = 0xAAAA
+ACK_PACKET_BYTES = (0xAAAA).to_bytes(2, byteorder='big')
 
 # The constant "0101010101010101" used as a header indicating the packet contains data.
-DATA_PACKET_HEADER = 0x5555
+DATA_PACKET_BYTES = (0x5555).to_bytes(2, byteorder='big')
 
-# The constant "1111111111111111" used as a header indicating the packet is a FIN.
-ONES_WORD = 0xFFFF
-
-# Number of bits in a byte.
-BYTE_SIZE = 8
-
-# Number of bits in a word.
-WORD_SIZE = 16
+# A FIN packet. 48 zeroes followed by 16 ones. "000000000000FFFF" in hex.
+FIN_PACKET = (0xFFFF).to_bytes( 8, byteorder='big' )
 
 # Constant value that serves as a placeholder header so ACK and data packets have
 # the same structure. Takes the place of the checksum in the data packet structure.
-ZERO_WORD = 0x0000
+ZERO_BYTES = (0).to_bytes(2, byteorder='big')
 
 ''' 
     Iterates through the paylod of a packet by 16-bit words and calculates 
     the "internet checksum" as specified in RFC 1071. 
 
-    @param  message: A bitarray of a packet
+    @param  message: payload of packet as a "bytes" object
     @return 16-bit checksum as an integer
 '''
 def calcChecksum( message ) :
+
     # Zeroes out the checksum to "0x0000".
-    checksum = ZERO_WORD
+    checksum = 0 
 
-    # If the message is not a multiple of 16 bits, its last 'word' is only 
-    # a single byte, so zeroes are appended to make it a full 16-bit word.
-    bitsLastWord = len( message ) % WORD_SIZE
-    if ( bitsLastWord == 8 ) :
-        message.insert( '0x00', len( message ) - BYTE_SIZE + 1 )
+    # Number of bytes in message.
+    msg_len = len( message )
 
+    # This is 1 (true) if the message has an odd number of bytes.
+    is_odd_length = msg_len % 2
+        
+    # Skips last 1 or 2 bytes depending on if msg_len is odd or even.
+    two_less = msg_len - 2
 
-    # If the last 'word' isn't a word (16 bits) or a 
-    # half-word (8 bits), something went wrong.
-    elif ( bitsLastWord != 0 ) :
-        print( "\nThis should never happen!\n" )
-        exit(1)
-
-    for word in message.cut(16) :
-        # Adds two 16-bit words. If there was an overflow, 1 is added to the result. 
-        result = checksum + word.int 
+    # Iterates through all 16-bit words, adding each to the current sum.
+    for i in range( 0, two_less , 2) :
+        intValueOfWord = ( message[ i ] << 8 ) + message[ i + 1 ]
+        result = checksum + intValueOfWord
         checksum = ( result & 0xFFFF ) + ( result >> 16 )
 
-    checksum = ~checksum
-    return checksum & 0xFFFF
+    # If the message has an odd number of bytes, the last byte is padded
+    # with 8 zeroes and added as the resultant 16-bit word to the checksum.
+    if ( is_odd_length ) :
+        intValueOfWord = message[ msg_len - 1]
+        result = checksum + intValueOfWord
+        checksum = ( result & 0xFFFF ) + ( result >> 16 )
+        
+    # If the message has an even number of bytes, the last two bytes are added normally.
+    else :
+        intValueOfWord = ( message[ msg_len - 2] << 8 ) + message[ msg_len - 1]
+        result = checksum + intValueOfWord
+        checksum = ( result & 0xFFFF ) + ( result >> 16 )
+
+    return ~checksum & 0xFFFF
 
 ''' 
     Given the sequence number of a received data packet, 
     constructs an ACK packet and returns it.
 
-    @param  seqNum: 32-bit sequence number 
-    @return ACK packet as a BitArray
+    @param  seqNum: integer sequence number 
+    @return ACK packet as a "bytes" object
 '''
 def buildDataPacket( payload, seqNum ) :
-    # Converts the sequence number to a 32-bit bitarray.
-    seqNumBits = BitArray( uint=seqNum, length=32 )
+    
+    # Turns the sequence number into a 4-byte representation.
+    seqNumBytes = seqNum.to_bytes(4, byteorder='big')
 
-    # Calculates the checksum and converts it to a 16-bit bitarray.
+    # Calculates the checksum, then converts it to a 2-byte representation.
     checksum = calcChecksum( payload )
+    checksumBytes = checksum.to_bytes(2, byteorder='big')
 
-    checksumBits = BitArray( uint=checksum, length=16 ) 
-
-    # Converts the DATA_PACKET_HEADER to a 16-bit bitarray.
-    dataPacketBits = BitArray( uint=DATA_PACKET_HEADER, length=16 )
-
-    # Builds the packet from its component headers and payload.
-    seqNumBits.append( checksumBits )
-    seqNumBits.append( dataPacketBits )
-    seqNumBits.append( payload )
-
-    return seqNumBits 
+    return b''.join((seqNumBytes, checksumBytes, DATA_PACKET_BYTES, payload))
 
 ''' 
     Given the sequence number of a received data packet, 
@@ -92,39 +83,17 @@ def buildDataPacket( payload, seqNum ) :
 '''
 def buildACKPacket( seqNum ) :
 
-    # Converts the sequence number to a 32-bit bitarray.
-    seqNumBits = BitArray( uint=seqNum, length=32 )
-
-    # Converts ZERO_BITS (0x0000) to a bitarray
-    zeroBits = BitArray( uint=ZERO_WORD, length=16 )
-
-    # Converts the ACK_PACKET_HEADER to a 16-bit bitarray.
-    ackPacketBits = BitArray( uint=ACK_PACKET_HEADER, length=16 )
-
-    
-    # Builds the packet from its component headers and payload.
-    seqNumBits.append( zeroBits )
-    seqNumBits.append( ackPacketBits )
-
-    return seqNumBits
+    seqNumBytes = seqNum.to_bytes(4, byteorder='big')
+    return b''.join((seqNumBytes, ZERO_BYTES, ACK_PACKET_BYTES))
 
 '''
-    Builds and returns a FIN packet used by the client to indicate no more
+    Returns a FIN packet used by the client to indicate no more
     data packets will be sent.
     
     Format is: 00000000
                0000FFFF
 
-    @return FIN packet as a BitArray
+    @return FIN packet as a "bytes" object
 '''
 def buildFINPacket() :
-    
-    # Creates a BitArray of 48 leading zeroes, as placeholders for the
-    # sequence number and checksum fields (FIN packet doesn't need them).
-    FINPacket = BitArray( uint=0, length=48 )
-
-    # Appends 16 ones 
-    zeroBits = BitArray( uint=1, length=16 )
-    FINPacket.append( zeroBits )
-
-    return FINPacket
+    return FIN_PACKET
