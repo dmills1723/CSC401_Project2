a1f66d402f1135c9319e52f0e919f44387254889
Jonathan Balliet <jrballiet@hotmail.com>
2019-03-27 19:45:07 -0400

    Cleaned up code in p2mpServer.py

--- p2mpServer.py
+++ p2mpServer.py
@@ -3,6 +3,7 @@ import socket
 import numpy as np
 import utils
 
+# Gets command line arguments
 def getArgs(args):
 
     if len(args) != 4:
@@ -18,6 +19,8 @@ def getArgs(args):
 # Server port, file name to write, probability loss of packet
 success, port, filename, p = getArgs(sys.argv)
 
+# If correct number of command line arguments
+# Assign them to their corresponding values
 if success:
     SERVER_PORT = int(port)
     FILE_NAME = filename
@@ -25,10 +28,11 @@ if success:
 else:
     sys.exit(1)
 
+# Creates the UDP socket and binds to the passed in port number
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(('', SERVER_PORT))
 
-print("socket success\n")
+print("Server is Running.\n")
 
 # is packet in-sequence
 inseq = False
@@ -54,37 +58,37 @@ ACK = None
 # if incorrect checksum, ACK for last received in-sequence packet
 prevACK = None
 
-
+# open file for writing
 f = open(FILE_NAME, 'wb')
 
+# Retrieve segments from the "sender" until the FIN packet has been sent (representing the entire file has been sent )
 try:
     while True:
-        data, addr = sock.recvfrom(1064) # buffer size is 1024 bytes
-        #print("Message:", data)
+        data, addr = sock.recvfrom(1064) # buffer size is 1064 bytes
 
         # check in-sequence
         seqnum = int.from_bytes(data[:4], byteorder='big')
 
         # generate random number for packet loss
         rand = np.random.uniform(0, 1)
-        #print(rand)
 
+        # if the random number is less than equal to probablity loss
+        # drop the packet and display the packet loss message
         if rand <= P_LOSS:
             print('Packet loss, sequence number = ', str(seqnum) )
 
         # continue if > p, otherwise drop packet
         if rand > P_LOSS:
             if data[6:8] == b'\xff\xff':
-                print('FIN packet received')
                 FIN = utils.buildFINPacket()
                 sock.sendto(FIN, addr)
                 break
 
-
             # compute checksum using utility function
             databits = data[8:]
             calccheck = utils.calcChecksum(databits)
-
+    
+            # checks if insequence and sets the boolean flag accordingly
             if seqnum == (prevseqnum + 1):
                 inseq = True
             else:
@@ -92,14 +96,14 @@ try:
 
             # check checksum value
             checksum = int.from_bytes( data[4:6], byteorder='big' )
-            #print( checksum )
-            #print( calccheck )
+            
+            # compare checksum vs calculated checksum value
+            # set the boolean flag to whether or not they are equal
             if checksum == calccheck:
                 check = True
             else:
                 check = False
 
-            #print( "check %s" %check )
             # SUCCESS: if checksum correct and in-sequence, send ACK segment for packet to client (UDP)
             if inseq and check:
                 # send ACK for packet
@@ -115,21 +119,20 @@ try:
                 prevACK = utils.buildACKPacket(prevseqnum)
                 sock.sendto(prevACK, addr)
 
-        # if last packet is sent, exit
-        #if lastPacket:
-         #   FIN = utils.buildFINPacket()
-          #  sock.sendto(FIN, addr)
-           # break
-
-    f.flush()
+    # File has finished downloaded
+    print('File has finished downloading.')
+    
+    # Close all resources and exit the program
     sock.close()
+    f.flush()
     f.close()
     print("Successfully exited program\n")
-    #sys.exit(0)
-
+    
+# Keyboard Interrupt Exception occured.
+# Close all resources and exit the program
 except KeyboardInterrupt:
-    f.flush()
     sock.close()
+    f.flush()
     f.close()
     print("\nSuccessfully exited program\n")
     sys.exit(0)
