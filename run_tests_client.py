cd7b659ff284767bd0df8f2fda38b5a7eb094586
demills <demills@ncsu.edu>
2019-03-27 13:17:40 -0400

    Updated run_tests_client.py.

--- run_tests_client.py
+++ run_tests_client.py
@@ -4,7 +4,8 @@ import sys
 import subprocess
 
 PORT_NUM = "10000"
-FILE_TO_SEND = "big_file.txt"
+#FILE_TO_SEND = "big_file.txt"
+FILE_TO_SEND = "small_file.txt"
 NUM_SERVERS = len( sys.argv ) - 1
 
 ##################
@@ -18,19 +19,24 @@ servers = sys.argv[1:6]
 INITIAL_ARGS = [ "python3", "p2mpclient.py" ]
 
 current_args = []
-for run in range( 5 ) :
-    for num_servers_per_run in range( NUM_SERVERS ) :
+for num_servers_per_run in range( NUM_SERVERS ) :
+    for run in range( 5 ) :
+        # Builds argument list for one run of the test.
         current_args.clear() 
         current_args += INITIAL_ARGS
         current_args += servers[0:num_servers_per_run + 1]
         current_args += [ PORT_NUM, FILE_TO_SEND, TASK_1_MSS ]
-        print( current_args )
-        start = time.time()
-        subprocess.run( current_args )
-        end = time.time()
-        print( "TASK:1,RUN:%d,NUMSERVERS:%d,TIME:%f" %( run + 1, num_servers_per_run + 1, end - start ))
-        input( "Press enter to run next test." )
 
+        start = time.time()
+        #subprocess.run( current_args )
+        print( "RUN %d, NUM_SERVERS %d\n" %(run + 1, num_servers_per_run + 1))
+        print( current_args )
+        end = time.time()
+        print( "TASK:1,RUN:%d,NUMSERVERS:%d,TIME:%f" %( run + 1, num_servers_per_run + 1, end - start ), file=open("test_output.txt", "a" ))
+        print( "Sleeping for next test" )
+        time.sleep( 3 )
+
+exit()
 ##################
 ##### Task 2 #####
 ##################
