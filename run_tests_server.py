a4092b7f97235fb074d3f2e651b4b7aae50b23f1
demills <demills@ncsu.edu>
2019-03-27 13:44:31 -0400

    Changed ouput file name in run_test_server.py

--- run_tests_server.py
+++ run_tests_server.py
@@ -7,7 +7,7 @@ run_num = 0
 current_args = []
 INITIAL_ARGS = [ "python3", "p2mpServer.py", LISTENING_PORT, PROB_LOSS ]
 while True :
-	file_name = "textbook_run" + str(run_num) + ".txt"
+	file_name = "output" + str(run_num) + ".txt"
 	run_num += 1
 	current_args.clear()
 	current_args = INITIAL_ARGS.copy()
